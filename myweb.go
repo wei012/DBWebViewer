8ff7199da553cf3d1d051eb42222ab379eedb920
Wei <tjmd012@gmail.com>
2018-01-21 11:09:47 +1000

    Implment the feature to hide specified display field

--- myweb.go
+++ myweb.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"reflect"
@@ -20,20 +21,28 @@ type Field struct {
 	Hide  bool
 }
 
-// ServerName is Mongo DB server host address
-var ServerName string
+// WebConf is the configuration for the web server
+type WebConf struct {
+	DBServer  string
+	MaxOutNum int
+	WebPort   string
+	DB        map[string]map[string]map[string]string
+}
 
-// DicTab is the detailed schema information for each table
-var DicTab map[string]map[string]map[string]Field
+// DicFields is the detailed schema information for each table
+var DicFields map[string]map[string]map[string]Field
 
-// MaxOutNum is the max returned records number
-var MaxOutNum int
+// DicWebConfs is the detailed configuration of each field
+var DicWebConfs WebConf
 
 func getHeaderStr(dbName string, tableName string) string {
-	record := DicTab[dbName][tableName]
+	record := DicFields[dbName][tableName]
 	data := ""
+	dicHides := getDisplayColumns(dbName, tableName)
 	for k := range record {
-		data += `{ "name": "` + k + `", "type": "text" },`
+		if _, has := dicHides[k]; !has {
+			data += `{ "name": "` + k + `", "type": "text" },`
+		}
 	}
 	if len(data) > 0 {
 		data = data[0 : len(data)-1]
@@ -51,8 +60,24 @@ func getSchema(dbName string, tableName string, w http.ResponseWriter, r *http.R
 	fmt.Fprint(w, string(data))
 }
 
+//Load the information of fields to hide
+func getDisplayColumns(dbName string, tableName string) map[string]bool {
+	dicHide := make(map[string]bool)
+	if db, hasDB := DicWebConfs.DB[dbName]; hasDB {
+		if tab, hasTab := db[tableName]; hasTab {
+			parts := strings.Split(tab["Hides"], ",")
+			for _, s := range parts {
+				if len(s) > 0 {
+					dicHide[s] = true
+				}
+			}
+		}
+	}
+	return dicHide
+}
+
 func getItemsStr(dbName string, tableName string, fields map[string][]string) string {
-	session, err := mgo.Dial(ServerName)
+	session, err := mgo.Dial(DicWebConfs.DBServer)
 	if err != nil {
 		panic(err)
 	}
@@ -61,19 +86,26 @@ func getItemsStr(dbName string, tableName string, fields map[string][]string) st
 	session.SetMode(mgo.Monotonic, true)
 	c := session.DB(dbName).C(tableName)
 
+	columns := bson.M{}
 	filter := bson.M{}
+
+	dicHides := getDisplayColumns(dbName, tableName)
 	for k, v := range fields {
 		if len(v) > 0 && len(v[0]) > 0 {
-			if DicTab[dbName][tableName][k].Type == "int" {
+			if DicFields[dbName][tableName][k].Type == "int" {
 				filter[k], _ = strconv.Atoi(v[0])
 			} else {
 				filter[k] = v[0]
 			}
 		}
+
+		if _, has := dicHides[k]; !has {
+			columns[k] = 1
+		}
 	}
 
 	var records []bson.M
-	err = c.Find(filter).Limit(MaxOutNum).All(&records)
+	err = c.Find(filter).Select(columns).Limit(DicWebConfs.MaxOutNum).All(&records)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -92,7 +124,7 @@ func getItems(dbName string, tableName string, w http.ResponseWriter, r *http.Re
 }
 
 func loadTab(dbName string, tableName string) {
-	session, err := mgo.Dial(ServerName)
+	session, err := mgo.Dial(DicWebConfs.DBServer)
 	if err != nil {
 		panic(err)
 	}
@@ -107,16 +139,16 @@ func loadTab(dbName string, tableName string) {
 		log.Fatal(err)
 	}
 
-	if _, exist := DicTab[dbName]; !exist {
-		DicTab[dbName] = make(map[string]map[string]Field)
+	if _, exist := DicFields[dbName]; !exist {
+		DicFields[dbName] = make(map[string]map[string]Field)
 	}
 
-	if _, exist := DicTab[dbName][tableName]; !exist {
-		DicTab[dbName][tableName] = make(map[string]Field)
+	if _, exist := DicFields[dbName][tableName]; !exist {
+		DicFields[dbName][tableName] = make(map[string]Field)
 	}
 
 	for k, v := range record {
-		DicTab[dbName][tableName][k] = Field{Type: reflect.TypeOf(v).String()}
+		DicFields[dbName][tableName][k] = Field{Type: reflect.TypeOf(v).String()}
 	}
 }
 
@@ -152,15 +184,31 @@ func dispatch(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func loadConf() bool {
+	content, err := ioutil.ReadFile("./conf/web.conf.json")
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	if err := json.Unmarshal(content, &DicWebConfs); err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	DicFields = make(map[string]map[string]map[string]Field)
+	return true
+}
+
 func main() {
 
-	ServerName = "127.0.0.1"
-	MaxOutNum = 100
-	DicTab = make(map[string]map[string]map[string]Field)
-
+	fmt.Println("Loading configuration...")
+	if !loadConf() {
+		fmt.Println("Failed to load configuration file")
+		return
+	}
 	fmt.Println("Starting server...")
 	http.HandleFunc("/", dispatch)
-
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Fatal(http.ListenAndServe(":"+DicWebConfs.WebPort, nil))
 
 }
